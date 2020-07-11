33a162c03edd37a6083178a6a203cf941f5bf784
ozashu <bm0801667@gmail.com>
2020-07-12 05:10:11 +0900

    add rehype-highlight.js

--- lib/posts.ts
+++ lib/posts.ts
@@ -3,6 +3,8 @@ import path from 'path'
 import matter from 'gray-matter'
 import remark from 'remark'
 import html from 'remark-html'
+// @ts-ignore
+import highlight from 'rehype-highlight';
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
@@ -57,6 +59,7 @@ export async function getPostData(id: string) {
   // Use remark to convert markdown into HTML string
   const processedContent = await remark()
     .use(html)
+    .use(highlight)
     .process(matterResult.content)
   const contentHtml = processedContent.toString()
 
